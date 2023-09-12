c1a832ca35541eb99dab61d048715bae02b9e492
tmitsuhiro <kmtsjp@gmail.com>
2023-09-12 11:07:14 -0300

    novo comit

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -6,6 +6,7 @@ import os
 
 app = Flask(__name__)
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://banco_ponterest_e0g1_user:9zLR6pEoY9jBHgGeCE6kQVnpULBH6nm2@dpg-cjvs87fhdsdc73a0m4og-a.oregon-postgres.render.com/banco_ponterest_e0g1"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config['SECRET_KEY'] = "11097aa57aae4ca7bd64d5bef4499521"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
